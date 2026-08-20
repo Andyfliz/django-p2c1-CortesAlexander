2fa00f75961836d295e31c180a8524c1505295dd
Andyfliz <alexander1704aco@gmail.com>
2026-08-20 10:26:31 -0400

    Agregación de archivos clase 3

---
+++ dispositivos/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+app_name = "dispositivos"
+
+urlpatterns = [
+    path("", views.inicio, name="inicio"),
+
+    path(
+        "zonas/<int:zona_id>/dispositivos/",
+        views.dispositivos_zona,
+        name="por_zona",
+    ),
+
+    path(
+        "dispositivos/<int:dispositivo_id>/",
+        views.dispositivo_numero,
+        name="por_dispositivo",
+    ),
+]
---
+++ dispositivos/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+def inicio(request):
+    return HttpResponse(
+        "<h1>EcoEnergy</h1>"
+        "<p>Back End en funcionamiento</p>"
+    )
+def dispositivos_zona(request, zona_id):
+    if zona_id != 3:
+        return HttpResponse(
+            "Zona no encontrada", status=404
+        )
+    return HttpResponse(
+        f"Dispositivos de la zona {zona_id}"
+    )
+
+def dispositivo_numero(request, dispositivo_id):
+    if dispositivo_id != 2:
+        return HttpResponse(
+            "Dispositivo no encontrado", status=404
+        )
+    return HttpResponse(
+        f"Numero de dispositivo {dispositivo_id}"
+    )
