a6415d9f5188a6d079bdd0889ee5863b4bdf2bfd
Alexander <alexander1704aco@gmail.com>
2026-09-14 20:29:44 -0300

    feat: Correcion de setting y Creacion de modelos organization, devices y department

--- organizations/models.py
+++ organizations/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from core.models import BaseModel
 
 # Create your models here.
+class Organization(BaseModel):
+    name = models.CharField(max_length=150)
+    tax_id = models.CharField(max_length=20, unique=True)
+    is_activate = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Department(BaseModel):
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.PROTECT,
+        related_name="departments"
+    )
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
